cbaa23526a862983a8837b44a54e4630d057c0ad
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-08-10 17:55:56 +0200

    Bootstrap the iOS SwiftUI app template
    This is the default template offered by XCode without Core Data or any of that.

---
+++ ios-app/ios-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-app
+//
+//  Created by Tomas Hubelbauer on 10.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-app/ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Tomas Hubelbauer on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
