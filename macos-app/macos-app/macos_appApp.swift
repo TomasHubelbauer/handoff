c23da826c25c07676d2d5f7e1fc811c54ecdb00e
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-08-10 17:56:06 +0200

    Bootstrap the macOS SwiftUI app template
    This is the default template offered by XCode without Core Data or any of that.

---
+++ macos-app/macos-app/macos_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_appApp.swift
+//  macos-app
+//
+//  Created by Tomas Hubelbauer on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct macos_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
